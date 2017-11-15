386b475f1623d534e8b8a733e3fa48980e9ef4bc
khawari-md <aridzakwan@gmail.com>
2017-11-16 05:31:04 +0700

    fix

---
+++ soal3.c
@@ -0,0 +1,163 @@
+#define FUSE_USE_VERSION 28
+#define HAVE_SETXATTR
+
+#ifdef HAVE_CONFIG_H
+#include <config.h>
+#endif
+
+#ifdef linux
+/* For pread()/pwrite() */
+#define _XOPEN_SOURCE 500
+#endif
+
+#include <fuse.h>
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <dirent.h>
+#include <errno.h>
+#include <sys/time.h>
+#ifdef HAVE_SETXATTR
+#include <sys/xattr.h>
+#endif
+
+static const char *dirpath = "/home/administrator/Downloads/";
+
+static int xmp_getattr(const char *path, struct stat *stbuf)
+{
+	int res;
+	char fpath[1000];
+
+	sprintf(fpath,"%s%s",dirpath, path);
+	res = lstat(fpath, stbuf);
+	if (res == -1)
+		return -errno;
+
+	return 0;
+}
+
+static int xmp_readdir(const char *path, void *buf, fuse_fill_dir_t filler,
+		       off_t offset, struct fuse_file_info *fi)
+{
+	DIR *dp;
+	struct dirent *de;
+
+	(void) offset;
+	(void) fi;
+
+	char fpath[1000];
+
+	sprintf(fpath,"%s%s",dirpath, path);
+
+	dp = opendir(fpath);
+	if (dp == NULL)
+		return -errno;
+
+	while ((de = readdir(dp)) != NULL) {
+		struct stat st;
+		memset(&st, 0, sizeof(st));
+		st.st_ino = de->d_ino;
+		st.st_mode = de->d_type << 12;
+		if (filler(buf, de->d_name, &st, 0))
+			break;
+	}
+
+	closedir(dp);
+	return 0;
+}
+
+static int xmp_read(const char *path, char *buf, size_t size, off_t offset,
+		    struct fuse_file_info *fi)
+{
+	int fd;
+	int res;
+	char fpath[1000];
+
+	sprintf(fpath,"%s%s",dirpath, path);	
+
+	(void) fi;
+	
+	fd = open(fpath, O_RDONLY);
+	if (fd == -1)
+		return -errno;
+
+	res = pread(fd, buf, size, offset);
+	if (res == -1)
+		res = -errno;
+
+	close(fd);
+	return res;
+}
+
+static int xmp_truncate(const char *path, off_t size)
+{
+	int res;
+
+	char fpath[1000];
+
+	sprintf(fpath,"%s%s",dirpath, path);
+
+	res = truncate(fpath, size);
+	if (res == -1)
+		return -errno;
+
+	return 0;
+}
+
+static int xmp_write(const char *path, const char *buf, size_t size,
+		     off_t offset, struct fuse_file_info *fi)
+{
+	int fd,fe;
+	int res;
+
+	char fpath[1000],gpath[1000],dir[100];
+
+	sprintf(fpath,"%s%s",dirpath, path);
+
+	strncpy(dir, dirpath, strlen(dirpath));
+	strcat(dir, "simpanan/");
+
+	sprintf(gpath,"%s%s",dir, path);
+
+	(void) fi;
+	fd = open(fpath, O_WRONLY);
+	if (fd == -1)
+		return -errno;
+
+	FILE *fptr;
+
+	struct stat st = {0};
+	if (stat("/home/administrator/Downloads/simpanan", &st) == -1) {
+   			mkdir("/home/administrator/Downloads/simpanan", 0777); //buat directory jika belum ada
+		}
+
+    fptr = fopen(gpath, "w+");
+
+    fe = open(gpath, O_WRONLY);
+	if (fe == -1)
+		return -errno;
+
+	res = pwrite(fe, buf, size, offset);
+	if (res == -1)
+		res = -errno;
+
+	close(fd);
+	close(fe);
+	fclose(fptr);
+	return res;
+}
+
+static struct fuse_operations xmp_oper = {
+	.getattr	= xmp_getattr,
+	.readdir 	= xmp_readdir,
+	.read 		= xmp_read,
+	.write 		= xmp_write,
+	.truncate 	= xmp_truncate,
+};
+
+int main(int argc, char *argv[])
+{
+	umask(0);
+	return fuse_main(argc, argv, &xmp_oper, NULL);
+}
